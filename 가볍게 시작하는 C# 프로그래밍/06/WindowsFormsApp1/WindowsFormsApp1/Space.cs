b35afd32aaaab60b3143f000bfb02681794693c0
yeesonmin <yeesonmin@naver.com>
2020-02-20 10:46:37 +0900

    Add Project[06] (다가오는 별 그리기)

---
+++ 가볍게 시작하는 C# 프로그래밍/06/WindowsFormsApp1/WindowsFormsApp1/Space.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Space : Form
+    {
+        Star[] stars;
+        public Graphics g;
+        public int countStar = 50;
+
+        public Space()
+        {
+            InitializeComponent();
+            StartStar();
+            timer1.Start();
+        }
+
+        void StartStar()
+        {
+            g = this.CreateGraphics();
+            stars = new Star[countStar];
+
+            for(int n = 0; n<stars.Length;n++)
+            {
+                stars[n] = new Star(this, n);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int n = 0; n< stars.Length;n++)
+            {
+                stars[n].Move();
+            }
+        }
+    }
+}
---
+++ 가볍게 시작하는 C# 프로그래밍/06/WindowsFormsApp1/WindowsFormsApp1/Star.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class Star
+    {
+        Space frmMain;
+        Random r;
+        Color color;
+        int cx, cy, radius; //radius : 중심 점과 별의 거리
+        double theta;
+        float size;
+
+        public Star()
+        {
+
+        }
+
+        public Star(Space parents, int i)
+        {
+            frmMain = parents;
+
+            cx = parents.Width / 2;
+            cy = parents.Height / 2;
+
+            StartStar(i);
+        }
+
+        void StartStar(int i)
+        {
+            int degree;
+            r = new Random(i);
+            radius = r.Next(0, GetLongestDistance());
+            color = Color.FromArgb(255, r.Next(255), r.Next(255), r.Next(255));
+
+            degree = (360 / frmMain.countStar) * i;
+            theta = degree * Math.PI / 180;
+        }
+
+        public void Move()
+        {
+            Draw(new SolidBrush(Color.Black));
+
+            radius += 1 + (radius / 10);
+            size = 1 + (radius / 50);
+
+            Draw(new SolidBrush(color));
+
+            if(radius>GetLongestDistance())
+            {
+                radius = r.Next(GetLongestDistance());
+            }
+
+        }
+
+        void Draw(SolidBrush b)
+        {
+            double x, y;
+
+            x = cx + radius * Math.Cos(theta);
+            y = cy + radius * Math.Sin(theta);
+
+            frmMain.g.FillEllipse(b, (float)x - size / 2, (float)y - size / 2, (float)size, (float)size);
+
+        }
+        int GetLongestDistance()
+        {
+            return (int)(Math.Sqrt(Math.Pow(cx,2) + Math.Pow(cy,2)));
+        }
+    }
+}
