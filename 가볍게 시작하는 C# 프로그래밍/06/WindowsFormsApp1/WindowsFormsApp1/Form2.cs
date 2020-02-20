5924ff82ca39d1a75617ed0b3ce4b29ef5d651a0
yeesonmin <yeesonmin@naver.com>
2020-02-20 09:31:18 +0900

    Add Project[06] (곡선 그래프 그리기)

---
+++ 가볍게 시작하는 C# 프로그래밍/06/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Pen p = new Pen(Color.White);
+
+            for(float i = 1; i<50; i++)
+            {
+                g.DrawLine(p, 0, i * 10, i * 10, this.Height);
+            }
+            g.Dispose();
+        }
+    }
+}
