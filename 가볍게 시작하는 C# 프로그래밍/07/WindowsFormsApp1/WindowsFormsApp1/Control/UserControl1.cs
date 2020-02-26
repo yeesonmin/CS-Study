469ba2e3ea8158bff1212d6e10cc83339f8a0628
yeesonmin <yeesonmin@naver.com>
2020-02-26 11:57:56 +0900

    Add Project[07] (사용자 정의 컨트롤 연습)

---
+++ 가볍게 시작하는 C# 프로그래밍/07/WindowsFormsApp1/WindowsFormsApp1/Control/UserControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Control
+{
+    public partial class UserControl1 : UserControl
+    {
+        Button[] buttons = new Button[3];
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            for(int i = 0; i<buttons.Length;i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Text = (i + 1).ToString() + "번째 버튼";
+                buttons[i].Size = new Size((int)((this.Width * 0.8) / 3), (int)((this.Height * 0.6) - label1.Height));
+                buttons[i].Location = new Point(
+                    (((this.Width - (buttons[i].Width * buttons.Length)) / 4) * (i + 1)) + (buttons[i].Width) * i,
+                    (this.Height - (label1.Height + label1.Location.Y)) / 2);
+                buttons[i].BackColor = Color.White;
+                
+                this.Controls.Add(buttons[i]);
+            }
+        }
+
+        public bool setButtonsText(int index, string text)
+        {
+            buttons[index].Text = text;
+            return true;
+        }
+    }
+}
---
+++ 가볍게 시작하는 C# 프로그래밍/07/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            userControl11.setButtonsText(0, "1빠");
+            userControl11.setButtonsText(1, "2빠");
+            userControl11.setButtonsText(2, "3빠");
+
+        }
+    }
+}
